f8893cd3702516a8e4953d46b30ea902d78dfcd8
kauandlopes <lopeskauandias@gmail.com>
2025-10-24 17:12:05 -0300

    feat: Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+    </head>
+    <body class="flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        <h1> TARRPT </h1>
+    </body>
+</html>
